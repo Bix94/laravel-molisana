1d93551cf12a5be683a828d929c423bbcb7e4dd7
Giacomo <giacomobiffi94@gmail.com>
2021-03-11 16:50:41 +0100

    consegna es

--- molisana/resources/views/products.blade.php
+++ molisana/resources/views/products.blade.php
@@ -7,13 +7,13 @@
   @foreach ($formati as $tipo => $formato)
     <h2> {{ $tipo }} </h2>
   <div class="schede">
-    @foreach ($formati as $k => $pasta)
+    @foreach ($formato as $index => $pasta)
     <div class="scheda">
       <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
       <div class="over">
         {{-- collego il [titolo] alla rotta/link pagina dettagli --}}
         {{-- devo passare ID come segnaposto => KEY per ottenere solo un link per ogni prodotto  --}}
-        <a href="{{ route('pagina-dettagli', [$id => $k]) }}"> {{ $pasta['titolo'] }} </a>
+        <a href="{{ route('pagina-dettagli', ['id' => $index]) }}"> {{ $pasta['titolo'] }} </a>
       </div>
     </div>
   @endforeach
